4e92204b61980681bc1ee2cde7730d0920cbcf18
mirela777 <70278105+mirela777@users.noreply.github.com>
2021-06-22 11:28:13 +0200

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,19 @@
+var words= new Array();
+function add_words(){
+var word = document.getElementById("text").value;
+ words.push(word.toLowerCase());
+}
+
+function search(){
+    var w = document.getElementById("text2").value;
+    var x = 0;
+    var txt ;
+    words.forEach(function(words){
+        if(w === words){
+           txt = "The searched word exists in the dictionary.";    
+    }
+        else{
+        txt = "The searched word does not exists in the dictionary.";
+        }})
+     document.getElementById("txt").innerHTML = txt;  
+}
