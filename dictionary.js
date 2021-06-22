d0d7090c01fdb1e297ac94b8298af025a7f9acb7
mirela777 <70278105+mirela777@users.noreply.github.com>
2021-06-22 11:34:04 +0200

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,18 +1,18 @@
-var words= new Array();
+var words = new Array();
 function add_words(){
-var word = document.getElementById("text").value;
- words.push(word.toLowerCase());
+        var word = document.getElementById("text").value;
+        words.push(word.toLowerCase());
 }
 
 function search(){
-    var w = document.getElementById("text2").value;
-    var txt ;
-    words.forEach(function(words){
-        if(w === words){
-           txt = "The searched word exists in the dictionary.";    
-    }
-        else{
-        txt = "The searched word does not exists in the dictionary.";
-        }})
-     document.getElementById("txt").innerHTML = txt;  
+        var w = document.getElementById("text2").value;
+        var txt ;
+        words.forEach(function(words){
+              if(w === words){
+                 txt = "The searched word exists in the dictionary.";    
+              }
+              else{
+                  txt = "The searched word does not exists in the dictionary.";
+            }})
+        document.getElementById("txt").innerHTML = txt;  
 }
